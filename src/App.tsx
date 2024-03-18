a5c557a7e3ab73f4ca63438f8940e292bcea05ed
keejin <yoplocheo@gmail.com>
2024-03-18 22:00:41 +0800

    set up nativewind for styling

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { registerRootComponent } from 'expo';
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className='text-lg'>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+registerRootComponent(App);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 // tailwind.config.js
 module.exports = {
   content: [
-    "./screens/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{ts,tsx}", "./src/*.{ts,tsx}"
   ],
-  // ...
+  theme: {
+    extend: {},
+  },
+  plugins: [],
 };
